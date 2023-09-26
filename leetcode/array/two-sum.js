4cae22951afc1614ab697843b9b1884c4952af6a
joaoprecode <joaocruzbs@gmail.com>
2023-09-26 16:23:23 -0300

    adjusts comments and variables

--- leetcode/array/two-sum.js
+++ leetcode/array/two-sum.js
@@ -1,31 +1,35 @@
 // brute force
 // this resolution use two loops
 
-function twoSum(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
+function useBruteForce(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
         return [i, j];
       }
     }
   }
 }
 
-/** use hash table
+// Ex: useBruteForce([1, 2, 3], 4) -> return [0, 2]
+
+/** use hash tables
  This resolution use one loop, 
  I create a map by transforming the value into an index, 
  When trying to access this map with complement value and it is not undefined, return a response
 */
-function useHashTable(nums, target) {
+function useHashTable(numbers, target) {
   let map = {};
-  for (let i = 0; i < nums.length; i++) {
-    let complement = target - nums[i];
+  for (let i = 0; i < numbers.length; i++) {
+    const complement = target - numbers[i];
     if (map[complement] !== undefined) {
       return [map[complement], i];
     }
-    map[[nums[i]]] = i;
+    map[[numbers[i]]] = i;
   }
 }
+
+// Ex: useHashTable([1, 2, 3], 4) -> return [0, 2]
 
 /** use hash table with method Map
  This resolution use one loop, 
@@ -33,13 +37,15 @@ function useHashTable(nums, target) {
  When trying to access this map with complement value and it is not undefined, return a response
 */
 
-function useMethodMap(nums, target) {
+function useHashTableWithMethodMap(numbers, target) {
   const map = new Map();
-  for (let i = 0; i < nums.length; i++) {
-    let complement = target - nums[i];
+  for (let i = 0; i < numbers.length; i++) {
+    const complement = target - numbers[i];
     if (map.has(complement)) {
       return [map.get(complement), i];
     }
-    map.set(nums[i], i);
+    map.set(numbers[i], i);
   }
 }
+
+// Ex: useHashTableWithMethodMap([1, 2, 3], 4) -> return [0, 2]
